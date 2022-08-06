1510a18898d950949fe70cd6b5969b08864f87c6
RicAlc <alcala.ricardoc@gmail.com>
2022-08-06 10:31:59 -0500

    8. Creando router

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  createStudent,
+  deleteStudent,
+  getAllStudents,
+  getStudent,
+  updateStudent,
+} from '../controllers/StudentController';
+
+const router = express.Router();
+
+router.get('/', getAllStudents);
+router.get('/:id', getStudent);
+router.post('/', createStudent);
+router.put('/:id', updateStudent);
+router.delete('/:id', deleteStudent);
+
+export default router;
