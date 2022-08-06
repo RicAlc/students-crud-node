e2f3682cf74ec5daab10c576090cb7bd521bcb5e
RicAlc <alcala.ricardoc@gmail.com>
2022-08-06 09:44:10 -0500

    6. Creando modelo StudentModel

---
+++ models/StudentModel.js
@@ -0,0 +1,10 @@
+import { DataTypes } from 'sequelize/types';
+import { db } from '../db/db';
+
+export const StudentModel = db.define('students', {
+  idST: { type: DataTypes.INTEGER, primaryKey: true },
+  nameST: { type: DataTypes.STRING },
+  lastNameST: { type: DataTypes.STRING },
+  tuitonCode: { type: DataTypes.STRING },
+  activeST: { type: DataTypes.BOOLEAN },
+});
